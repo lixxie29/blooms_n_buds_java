b45f6133477c6b88ff8678423fe5183dc8e92536
Crina Ciocan <84138256+lixxie29@users.noreply.github.com>
2024-09-22 20:48:35 +0300

    Add files via upload

---
+++ rest/rest/src/test/java/com/lix/java_flower_app/rest/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lix.java_flower_app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
